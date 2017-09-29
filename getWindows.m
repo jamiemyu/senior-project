622d12e55536873584913e9dc5c5b96d88e6585e
Jamie <jamieyu96@gmail.com>
2017-09-29 14:40:44 -0400

    Refactored demo code by integrating helper functions and organizing plotting section

--- getWindows.m
+++ getWindows.m
@@ -1,14 +1,17 @@
 % Function that returns a cell array of data organized into windows.
 % sampleArr is the array of samples being separated into windows
 % windowDuration is the size of the window
-function windowArr = getWindows(sampleArr, windowDuration, Fs)
+function [tArr, windowArr] = getWindows(sampleArr, windowDuration, Fs)
 numSamplesPerWindow = Fs * windowDuration;
 numSamples = length(sampleArr) / numSamplesPerWindow;
 
+tArr = cell(1, numSamples);
 windowArr = cell(1, numSamples);
 for i = 1:numSamples
     startIndex = (i * numSamplesPerWindow) - numSamplesPerWindow + 1;
     endIndex = (i * numSamplesPerWindow);
+    
+    tArr{i} = [startIndex:endIndex];
     windowArr{i} = sampleArr(startIndex:endIndex);
 end
 
