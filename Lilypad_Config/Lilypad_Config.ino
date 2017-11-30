62642493e4adde977fae538deabc96246a29ad79
Varun Garg <vxg130@case.edu>
2017-11-29 23:58:22 -0500

    Final code
    Based on testing conducted in lap on the uno

--- Lilypad_Config/Lilypad_Config.ino
+++ Lilypad_Config/Lilypad_Config.ino
@@ -1,51 +1,81 @@
+
 #include <SD.h>
 
+const int buttonPin = 2;     // the number of the switch to activate
+const int analogPin = 3;     // analog pin where EEG voltage is recorded
+
+// variables will change:
+int val;
+int buttonState = 0;       // variable for reading switch status
+bool start = true;
+unsigned long starttime = 0;
+int array[270];
+int i;
+int j;
+
 File myFile; // File that is created on SD card
-int dataPoints = 0; // Total number of data recorded in one second
-const int analogEEGPin = 3; // Analong pin which is connected to EEG amplifier circuit
-int val = 0; // EEG reading from analog pin
-const int buttonPin = 2;
 
-int buttonState = 0; // variable to read push button status
-
-String fileName = "data";
-int nFiles = 0;
 
 void setup() {
-  
-  Serial.begin(9600);
-  Serial.print("Initilaizing SD Card: ");
+
+  // Debug: Serial.begin(115200);
+  // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
   //set pin 10 to ouput to write to SD card Breakout
   pinMode(10, OUTPUT);
   if (!SD.begin(10)) {
-    Serial.println("Initialization failed!");
+    // Debug: Serial.println("Initialization failed!");
     return;
   }
-  Serial.println("Initialization successful");
-  while(!SD.exists(fileName + ".txt")) {
-    nFiles++;
+  
+  if(SD.exists("data.txt")) {
+    SD.remove("data.txt");
   }
+  
+  myFile = SD.open("data.txt", FILE_WRITE);
+  myFile.close();
+  // Debug: Serial.println("Initialization successful");
+  starttime = millis();
+  i=0;
 }
 
 void loop() {
-  // read buton state
+
   buttonState = digitalRead(buttonPin);
-
-  if(buttonState == HIGH) {
-    myFile = SD.open(fileName + nFiles + ".txt", FILE_WRITE);
-    if(!myFile) {
-      Serial.println("Error opening data.txt");
-      return;
-    }
-    val = analogRead(analogEEGPin);
-    myFile.println(val);
-    myFile.close();
-  }
   
-  // 1/250 seconds = 3750 microseconds
-  delayMicroseconds(4000);
+  if (buttonState == LOW) {
+    // used to measure the time
+    if(start) {
+      starttime = millis();
+      start = false;
+    } 
+    val = analogRead(analogPin);
+    float voltage = val * (5.0 / 1023.0);
+    if(i < 250) {
+      array[i] = val;
+      i++;
+    }
+    
+    Serial.println(voltage);
+    // we use 935ms as we give 65ms for file data write
+    if((millis() - starttime)>= 935) {
+      start = true;
+      // Debug: starttime = millis();
+      // Debug: Serial.println("One second");
+      myFile = SD.open("data.txt", FILE_WRITE);
+      for(j = 0; j < i + 1; j++) {
+        myFile.println(array[j]);
+      }
+      // -1 in the file marks a second of data
+      myFile.println("-1");
+      myFile.close();
+      j=0;
+      i=0; 
+      // Debug: int stoptime = millis() - starttime;
+      // Debug: Serial.println("Time to write");
+      // Debug: Serial.println(stoptime);
+    }
+  
+  }
+  delayMicroseconds(3500);
 }
-
-
-
